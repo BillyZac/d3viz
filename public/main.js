d6d872c5d26693c5220c367a77168f0a1db71c34
Zachary Smith <billyzacsmith@gmail.com>
2016-03-19 15:11:20 -0600

    Circle attributes can be set using url params.
    Example:
    http://localhost:3333/?&color=red&radius=10
    
    This results in red circles with radius of 10. If no param is present, the visualization falls back to default values.

--- public/main.js
+++ public/main.js
@@ -1,29 +1,10 @@
 var circle = d3.selectAll('circle')
 
-circle.style("fill", "steelblue");
-circle.attr("r", 30);
+console.log(QueryString.count);
+console.log(QueryString.color);
 
-var QueryString = function () {
-  // This function is anonymous, is executed immediately and
-  // the return value is assigned to QueryString!
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-        // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-        // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-        // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  }
-    return query_string;
-}()
+var circleColor = QueryString.color || "steelblue"
+var circleRadius = QueryString.radius || 30
 
-console.log(QueryString.c);
+circle.style("fill", circleColor);
+circle.attr("r", circleRadius);
