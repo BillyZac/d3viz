843bab1d19126167b562360af6e0a75ec503cba5
Zachary Smith <billyzacsmith@gmail.com>
2016-03-19 20:13:23 -0600

    Replace Three Little Circles with a swoopy design.
    The swoop changes shape depending on the "radius" url param.
    
    I tried to get the shape to center on the spot the screenshotter is aiming at. We'll see. Might have to fiddle with this.

--- public/main.js
+++ public/main.js
@@ -1,10 +1,35 @@
-var circle = d3.selectAll('circle')
+var input = QueryString.radius || 50 // Tweak a couple points
 
-console.log(QueryString.count);
-console.log(QueryString.color);
+var points = [
+  [480, 200],
+  [580, input * 5],
+  [680, 100],
+  [780, 300],
+  [180, 300],
+  [280, 100],
+  [380, input * 5]
+]
 
-var circleColor = QueryString.color || "steelblue"
-var circleRadius = QueryString.radius || 30
+var newPoints = points.map(function(coords) {
+  return coords.map(function(point) {
+    return point * 0.5
+  })
+})
+console.log(newPoints);
 
-circle.style("fill", circleColor);
-circle.attr("r", circleRadius);
+var line =
+  d3.svg.line()
+    .tension(0) // Catmull–Rom
+    .interpolate("cardinal-closed")
+
+var svg =
+  d3.select("body").append("svg")
+    .datum(newPoints)
+    .attr("width", 480)
+    .attr("height", 800)
+
+svg.append("path")
+   .attr("d", line)
+
+svg.append("path")
+   .attr("d", line)
